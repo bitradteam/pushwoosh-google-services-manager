159ff597b496b948cfa8e82e32bebefa26e80311
promonteiro <promonteiro@mc.pt>
2024-02-23 16:32:57 +0000

    Added logic to clean up old google services files

--- hooks/before_build_MoveGoogleServices.js
+++ hooks/before_build_MoveGoogleServices.js
@@ -2,6 +2,7 @@
 
 //global variables
 var appId = "";
+var utils = require("./utils");
 
 // required node modules
 var fs = require('fs');
@@ -12,27 +13,53 @@ var configFile = "config.xml";
 var xmlData = fs.readFileSync(configFile).toString('utf8');
 
 var n = xmlData.search(" id=\"");
-if(n > 0)
-{
+if (n > 0) {
   n += 5;
   var count = 0;
   var cont = true;
-  while(cont) {
-    if(xmlData[n+count] == "\"") {
+  while (cont) {
+    if (xmlData[n + count] == "\"") {
       cont = false;
     } else {
       count++;
     }
   }
-  appId = xmlData.substring(n, n+count);
+  appId = xmlData.substring(n, n + count);
   console.log("[PUSHWOOSH HELPER] App Identifier detected: " + appId);
+}
+
+/**
+ * Get the absolute path to the location that Google Services
+ * file should be placed, depending on the platform.
+ * @param {object} context Cordova context
+ * @returns {string} Absolute path to the location google
+ * services file must be placed
+ */
+function getGoogleServiceTargetDir(context) {
+  var platformPath = utils.getPlatformPath(context);
+  var platform = context.opts.plugin.platform;
+  switch (platform) {
+    case "android": {
+      var platformVersion = utils.getPlatformVersion(context);
+      var majorPlatformVersion = platformVersion.split(".")[0];
+      if (parseInt(majorPlatformVersion) >= 7) {
+        return path.join(platformPath, "app");
+      } else {
+        return platformPath;
+      }
+    }
+    case "ios":
+      return platformPath;
+    default:
+      return undefined;
+  }
 }
 
 //function to copy the file
 function copyGoogleServicesFile() {
   var srcFile = path.join("www/google-services", appId, "google-services.zip");
   console.log("[PUSHWOOSH HELPER] Source file path: " + srcFile);
-  if(fs.existsSync(srcFile)) {
+  if (fs.existsSync(srcFile)) {
     var buildPath = "platforms/android/app/src/main/assets/www/google-services";
     fs.mkdirSync(buildPath, { recursive: true });
     console.log("[PUSHWOOSH HELPER] File exists.");
@@ -41,4 +68,22 @@ function copyGoogleServicesFile() {
   }
 };
 
+//function to check if google services files already exist and dispose of them
+function cleanGoogleServicesFile() {
+  var targetDir = getGoogleServiceTargetDir(context);
+
+  // Check if files exist and delete them if found
+  var existingFiles = ["google-services.json", "GoogleService-Info.plist"];
+  existingFiles.forEach(function (file) {
+    var filePath = path.join(targetDir, file);
+    console.log("[PUSHWOOSH HELPER] File path:", filePath);
+    console.log("[PUSHWOOSH HELPER] File exists?:", fs.existsSync(filePath))
+    if (fs.existsSync(filePath)) {
+      fs.unlinkSync(filePath);
+      console.log("[PUSHWOOSH HELPER] Deleted existing file:", filePath);
+    }
+  });
+}
+
+cleanGoogleServicesFile();
 copyGoogleServicesFile();
