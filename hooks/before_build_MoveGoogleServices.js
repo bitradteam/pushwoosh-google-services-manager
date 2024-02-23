bb1095c9c529a68abe7348df9bce4d402266d80a
promonteiro <promonteiro@mc.pt>
2024-02-23 16:47:04 +0000

    Improved cleanGoogleServicesFile

--- hooks/before_build_MoveGoogleServices.js
+++ hooks/before_build_MoveGoogleServices.js
@@ -5,12 +5,12 @@ var appId = "";
 var utils = require("./utils");
 
 // required node modules
-var fs = require('fs');
-var path = require('path');
+var fs = require("fs");
+var path = require("path");
 
 // determine appId (read it from config.xml)
 var configFile = "config.xml";
-var xmlData = fs.readFileSync(configFile).toString('utf8');
+var xmlData = fs.readFileSync(configFile).toString("utf8");
 
 var n = xmlData.search(" id=\"");
 if (n > 0) {
@@ -69,21 +69,27 @@ function copyGoogleServicesFile() {
 };
 
 //function to check if google services files already exist and dispose of them
-function cleanGoogleServicesFile(context) {
-  var targetDir = getGoogleServiceTargetDir(context);
+function cleanGoogleServicesFile() {
+  module.exports = function (context) {
+    return new Promise(function (resolve, reject) {
 
-  // Check if files exist and delete them if found
-  var existingFiles = ["google-services.json", "GoogleService-Info.plist"];
-  existingFiles.forEach(function (file) {
-    var filePath = path.join(targetDir, file);
-    console.log("[PUSHWOOSH HELPER] File path:", filePath);
-    console.log("[PUSHWOOSH HELPER] File exists?:", fs.existsSync(filePath))
-    if (fs.existsSync(filePath)) {
-      fs.unlinkSync(filePath);
-      console.log("[PUSHWOOSH HELPER] Deleted existing file:", filePath);
-    }
-  });
+      var targetDir = getGoogleServiceTargetDir(context);
+
+      // Check if files exist and delete them if found
+      var existingFiles = ["google-services.json", "GoogleService-Info.plist"];
+      existingFiles.forEach(function (file) {
+        var filePath = path.join(targetDir, file);
+        console.log("[PUSHWOOSH HELPER] File path:", filePath);
+        console.log("[PUSHWOOSH HELPER] File exists?:", fs.existsSync(filePath))
+        if (fs.existsSync(filePath)) {
+          fs.unlinkSync(filePath);
+          console.log("[PUSHWOOSH HELPER] Deleted existing file:", filePath);
+        }
+      });
+      return resolve();
+    });
+  };
 }
 
-cleanGoogleServicesFile(context);
+cleanGoogleServicesFile();
 copyGoogleServicesFile();
