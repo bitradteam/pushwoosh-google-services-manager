4d015774d94c0d8f97eb78c8e0f85f4a0ef9ade5
promonteiro <promonteiro@mc.pt>
2024-02-23 18:04:25 +0000

    Improved hook

--- hooks/before_build_MoveGoogleServices.js
+++ hooks/before_build_MoveGoogleServices.js
@@ -4,25 +4,15 @@
 const fs = require("fs");
 const path = require("path");
 
-// Constants
-const CONFIG_FILE = "config.xml";
-const GOOGLE_SERVICES_DIR = "www/google-services";
-const BUILD_PATH = "platforms/android/app/src/main/assets/www/google-services";
-const GOOGLE_SERVICES_FILE = "google-services.zip";
-
 /**
  * Extracts the App Identifier from the config.xml file.
  * @returns {string} The App Identifier.
  */
 function extractAppId() {
-  try {
-    const xmlData = fs.readFileSync(CONFIG_FILE, "utf8");
-    const match = xmlData.match(/ id="([^"]+)"/);
-    return match ? match[1] : "";
-  } catch (error) {
-    console.error("Error reading config.xml:", error);
-    return null;
-  }
+  const configFile = "config.xml";
+  const xmlData = fs.readFileSync(configFile, "utf8");
+  const match = xmlData.match(/ id="([^"]+)"/);
+  return match ? match[1] : "";
 }
 
 /**
@@ -30,17 +20,14 @@ function extractAppId() {
  * @param {string} appId The App Identifier.
  */
 function copyGoogleServicesFile(appId) {
-  try {
-    const srcFile = path.join(GOOGLE_SERVICES_DIR, appId, GOOGLE_SERVICES_FILE);
-    if (fs.existsSync(srcFile)) {
-      fs.mkdirSync(BUILD_PATH, { recursive: true });
-      fs.copyFileSync(srcFile, path.join(BUILD_PATH, GOOGLE_SERVICES_FILE));
-      console.log("[PUSHWOOSH HELPER] Copied google-services.zip");
-    } else {
-      console.log("[PUSHWOOSH HELPER] Source file not found:", srcFile);
-    }
-  } catch (error) {
-    console.error("Error copying Google Services file:", error);
+  const srcFile = path.join("www/google-services", appId, "google-services.zip");
+  if (fs.existsSync(srcFile)) {
+    const buildPath = "platforms/android/app/src/main/assets/www/google-services";
+    fs.mkdirSync(buildPath, { recursive: true });
+    fs.copyFileSync(srcFile, path.join(buildPath, "google-services.zip"));
+    console.log("[PUSHWOOSH HELPER] Copied google-services.zip");
+  } else {
+    console.log("[PUSHWOOSH HELPER] Source file not found:", srcFile);
   }
 }
 
@@ -48,15 +35,15 @@ function copyGoogleServicesFile(appId) {
  * Deletes existing Google Services files if found.
  */
 function cleanGoogleServicesFiles() {
-  try {
-    const filePath = path.join(BUILD_PATH, GOOGLE_SERVICES_FILE);
+  const targetDir = 'source/platforms/android/app/';
+  const existingFiles = ["google-services.json", "GoogleService-Info.plist"];
+  existingFiles.forEach(file => {
+    const filePath = path.join(targetDir, file);
     if (fs.existsSync(filePath)) {
       fs.unlinkSync(filePath);
       console.log("[PUSHWOOSH HELPER] Deleted existing file:", filePath);
     }
-  } catch (error) {
-    console.error("Error deleting existing Google Services file:", error);
-  }
+  });
 }
 
 // Main function
